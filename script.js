f442d0f1af88c840a3dd11986b5805d86881b7d5
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-08-11 19:32:45 -0700

    add charset for each group of options

--- script.js
+++ script.js
@@ -1,39 +1,95 @@
 // Assignment code here
 function generatePassword() {
   let password = "";
+  let charIndex = 0;
+  let char = "";
+  const lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbersOptions = "0123456789";
+  const specialCharacterdsOptions = '!"#$%&()*+,-./:;<=>?@[]^_`{|}~' + "'";
+  let passwordCharactersOptions = "";
 
+  //Get the password lenght from the user
   let passwordLenght = prompt(
     "Choose a password lenght. Please add at least 8 characters and no more thant 128"
   );
 
+  //Validate if the password lenght have the criterial between 8 and 128
   if (passwordLenght < 8) {
     alert(
       "Your password lenght is too short, please select at least 8 characters."
     );
-    return;
   } else if (passwordLenght > 128) {
     alert(
       "Your password lenght is too big, please seleat no more than 128 characters."
     );
-    return;
+  } else {
+    //Get from the user if we wnat lowercase charaters in the password
+    const includeLowerCase = confirm(
+      "You want to inlcuye LowerCase characters?"
+    );
+    //If the user want lowerCase character and the first charater to  the password.
+    if (includeLowerCase) {
+      passwordCharactersOptions += lowerCaseOptions;
+      charIndex = Math.floor(Math.random() * 26);
+      char = lowerCaseOptions[charIndex];
+      password += char;
+    }
+    //Get from the user if we want uppercase charaters in the password
+    const includeUpperCase = confirm(
+      "You want to inlcuye UpperCase characters?"
+    );
+    //If the user want upperCase character and the first charater to  the password.
+    if (includeUpperCase) {
+      passwordCharactersOptions += upperCaseOptions;
+      charIndex = Math.floor(Math.random() * 26);
+      char = upperCaseOptions[charIndex];
+      password += char;
+    }
+    //Get from the user if we want numbers charaters in the password
+    const includeNumbers = confirm("You want to inlcuye Numbers characters?");
+    //If the user want numbers add the first number to  the password.
+    if (includeNumbers) {
+      passwordCharactersOptions += numbersOptions;
+      charIndex = Math.floor(Math.random() * 10);
+      char = numbersOptions[charIndex];
+      password += char;
+    }
+    //Get from the user if we want numbers charaters in the password
+    const includeSpecialCharacters = confirm(
+      "You want to inlcuye Special Characters characters?"
+    );
+    //If the user want Special Characters in the password, add the first special Character to the password.
+    if (includeSpecialCharacters) {
+      passwordCharactersOptions += specialCharacterdsOptions;
+      charIndex = Math.floor(Math.random() * 31);
+      char = specialCharacterdsOptions[charIndex];
+      password += char;
+    }
+
+    //Validate if the user have at least one set of characters for  the password.
+    if (
+      !includeLowerCase &&
+      !includeUpperCase &&
+      !includeNumbers &&
+      !includeSpecialCharacters
+    ) {
+      alert("You need select at least one option for your password.");
+    } else {
+      for (var i = password.length; i < passwordLenght; i++) {
+        //Add any of the option between 32 and 122
+        charIndex = Math.floor(
+          Math.random() * passwordCharactersOptions.length
+        );
+        char = passwordCharactersOptions[charIndex];
+
+        password += char;
+      }
+      return password;
+    }
   }
 
-  for (var i = 0; i < passwordLenght; i++) {
-    //TODO: Create a random number
-    //Special Characters from 32 to 47 add at least 1
-
-    //Numbers from 48 to 57 add at least 1
-
-    //Uppercase from  65 to 90  at least 1
-    //Lowercase letter from  97 to 122  at Least 1s
-    const code = getRandomArbitrary(32, 122);
-
-    const char = String.fromCharCode(code);
-
-    password += char;
-  }
-
-  return password;
+  return "";
 }
 
 //Generate a Random number between min and max
